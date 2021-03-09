07aa81eccde9905ac9f2a58aa342fcf8baec93c0
sansanee <sansaneea@gmail.com>
2021-03-09 10:36:01 -0500

    update

---
+++ controllers/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  const pageData = await getPage("home");
+  console.log(pageData);
+  //getPage().then();
+  res.render('index', { title: pageData.title, page: pageData });
+});
+
+const getPage = async function (pageKey) {
+  let connection = await db.getConnection();
+  // SELECT pageID, pageKey, title, content, dateModified FROM`page` WHERE pageKey = 'home'
+  const rows = await connection.query("SELECT pageID, pageKey,title,content,dateModified FROM`page` WHERE pageKey = ?",
+    [
+      pageKey
+    ]);
+
+  let data = rows[0];
+
+  connection.end();
+  return data;
+}
+
+
+module.exports = router;
